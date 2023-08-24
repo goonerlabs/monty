3760d7422114c3600b08864ec9c284ff046f3294
itsoluwatobby <itsoluwatobby@gmail.com>
2023-08-25 00:24:29 +0100

    pchar added

--- get_op_func.c
+++ get_op_func.c
@@ -20,6 +20,8 @@ void (*get_op_func(char *s))(stack_t **stack, unsigned int line_number)
 		{"nop", nop},
 		{"div", _div},
 		{"sub", sub},
+		{"mod", mod},
+		{"pchar", pchar},
 		{NULL, NULL}
 	};
 	while (op[i].opcode)
--- mod.c
+++ mod.c
@@ -1,17 +1,17 @@
 #include "monty.h"
 
 /**
- * _div - function that divides the second top element of
+ * _mod - function that divides the second top element of
  *	the stack by the top element of the stack.
  * @stack: pointer to the top node of stack_t.
  * @line_number: present working line number of a Monty bytecodes file.
 */
 
-void _div(stack_t **stack, unsigned int line_number)
+void mod(stack_t **stack, unsigned int line_number)
 {
 	if ((*stack) == NULL || (*stack)->next == NULL)
 	{
-		printf("L<%d>: can't div, stack too short\n", line_number);
+		printf("L<%d>: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->next->n == 0)
@@ -20,6 +20,6 @@ void _div(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	(*stack)->next->n /= (*stack)->n;
+	(*stack)->next->n %= (*stack)->n;
 	pop(stack, line_number);
 }
